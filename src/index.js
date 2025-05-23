50ed2dde4d2ef9589818fc4cd296352e759b8b22
Rahul Singhvi <rahul.singhvi@swedbank.se>
2025-05-23 11:47:44 +0200

    js-action-solution

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+require("dotenv").config();
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
+  const nameToGreet = process.env["USER_NAME"] || "World";
+  console.log(`Hello ${nameToGreet}!`);
+  core.setOutput("greeting", `Hello ${nameToGreet}!`);
 
-    const nameToGreet = core.getInput('user-name');
-    console.log(`Hello ${nameToGreet}!`);
-    core.setOutput("greeting", `Hello ${nameToGreet}!`);
+  const time = new Date().toTimeString();
+  core.setOutput("time", time);
 
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
 } catch (error) {
-
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
